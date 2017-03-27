3da422d4f1d665f31735619f51b295b747a9560f
Bobby Handley <bhandley24@gmail.com>
2017-03-27 13:10:51 -0600

    Delete forms.py

--- dealioApp/dealioApp/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from dealioApp.models import Promotion
-
-class addPromo(ModelForm):
-    class Meta:
-        model = Promotion
-        fields = ['title', 'description', 'rating']
-
